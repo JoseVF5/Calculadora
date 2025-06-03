7fe5aabade9c2da0c521c93d100c572d54a1e7dd
José <josevictorengcomp@gmail.com>
2025-06-03 01:58:32 -0300

    Finalização do projeto

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from View.Interface import Tela_principal
 from Controller.Data import CalucaldoraApp
 
-
+# Onde inicia a aplicação
 if __name__ == "__main__":
 
     # Inicializa primeiro a view como None, depois cria o controller com a view
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     view.controller = controller # Passa o controller para a view
     view.frames_botoes()
     view.criar_botoes() 
-    view.janela.mainloop()
+    view.janela.mainloop() # Inicia a janela principal da aplicação
